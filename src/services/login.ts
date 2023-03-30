99754845e00464ad03ae719da8bde6764661ada7
zhiying_mar <zhiying_mar@163.com>
2023-03-30 12:00:02 +0800

    feat:post

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,9 +1,9 @@
 import HttpMethods from ".";
 
-const baseUrl='/users'
+const baseUrl='/login'
 export const login=async()=>{
-    await new HttpMethods().get(baseUrl,{
-        // "username":"test1",
+    await new HttpMethods().post(baseUrl,{
+        "username":"test1",
         "password":"test1"
     }).then((res: any)=>{
         console.log(res);
